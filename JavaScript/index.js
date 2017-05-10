add8a26a6355ed0a583414b39d5a0a045bbe68fe
Vova Dakhniy <vova.dakhniy@gmail.com>
2017-05-10 19:16:04 +0300

    Added animation and gallery Jqury plugins to headings and pictures

---
+++ JavaScript/index.js
@@ -0,0 +1,18 @@
+$('.title').textillate({
+  in: { 
+    effect: 'fadeIn',    
+    delayScale: 5,
+    delay: 50,
+    sequence: true
+  }
+});
+
+$('.titletwo').textillate({
+  in: { 
+    effect: 'fadeIn',    
+    delayScale: 5,
+    delay: 50,
+    shuffle: true
+  }
+});
+
